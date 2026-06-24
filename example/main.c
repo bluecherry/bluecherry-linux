0a2bba8d93d733dced44b1921062116368e50b55
Daan Pape <daan@dptechnics.com>
2026-06-24 16:49:14 +0200

    Updated CMakeFiles so that the example reflects how the user app should include the BlueCherry library

--- example/main.c
+++ example/main.c
@@ -18,7 +18,7 @@
  * If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
  */
 
-#include <bclite.h>
+#include <bluecherry/bclite.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
